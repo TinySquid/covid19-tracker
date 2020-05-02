c34c866dc670d1fb11d5bb62d5dd37b87be1e607
Michael Nunes <tinysquid.git@gmail.com>
2020-05-02 16:54:39 -0700

    Add country picker dropdown and style

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function CountryPicker() {
+// Material
+import { NativeSelect, FormControl } from "@material-ui/core";
+
+// Styles
+import styles from "./CountryPicker.module.css";
+
+// API
+import { fetchCountries } from "../../api";
+
+function CountryPicker({ handleCountryChange }) {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    getCountries();
+  }, [setCountries]);
+
   return (
-    <div>
-      <h1>CountryPicker</h1>
-    </div>
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
   );
 }
 
