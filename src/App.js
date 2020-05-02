8bfbd822e4488031c6f76c2a0373cc9c33839577
Michael Nunes <tinysquid.git@gmail.com>
2020-05-02 16:55:11 -0700

    Finalize App.js, render cards / chart / dropdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Components
 import { Cards, Chart, CountryPicker } from "./components";
 
+// Styles
 import styles from "./App.module.css";
 
+// API
+import { fetchData } from "./api";
+
 function App() {
+  const [data, setData] = useState(null);
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      setData(await fetchData());
+    };
+
+    getData();
+  }, []);
+
+  const handleCountryChange = async (country) => {
+    setData(await fetchData(country));
+    setCountry(country);
+  };
+
+  if (!data) return <div>Loading...</div>;
+
   return (
     <div className={styles.container}>
-      <Cards />
-      <CountryPicker />
-      <Chart />
+      <CountryPicker handleCountryChange={handleCountryChange} />
+      <Cards data={data} />
+      <Chart country={country} />
     </div>
   );
 }
