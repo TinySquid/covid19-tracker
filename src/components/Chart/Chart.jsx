a446077253cec2667eefb8f41ba0cd0ee0a93a22
Michael Nunes <tinysquid.git@gmail.com>
2020-05-02 16:54:25 -0700

    Add Chart component and styles

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Chart() {
+// Chartjs
+import { Line } from "react-chartjs-2";
+
+// Styles
+import styles from "./Chart.module.css";
+
+// API
+import { fetchDailyData } from "../../api";
+
+function Chart(props) {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const getDailyData = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    getDailyData();
+  }, []);
+
+  // Disable linechart on mobile and or when data isn't available
+  const lineChart =
+    dailyData.length && window.innerWidth > 500 ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => date),
+          datasets: [
+            {
+              data: dailyData.map(({ confirmed }) => confirmed),
+              label: "Infected",
+              borderColor: "#483290",
+              backgroundColor: "rgba(72, 50, 144, 0.15)",
+              fill: true,
+            },
+            {
+              data: dailyData.map(({ deaths }) => deaths),
+              label: "Deaths",
+              borderColor: "#ee3425",
+              backgroundColor: "rgba(238, 52, 37, 0.45)",
+              fill: true,
+            },
+          ],
+        }}
+      />
+    ) : null;
+
   return (
-    <div>
-      <h1>Chart</h1>
+    <div className={styles.container}>
+      {props.country ? <h3>No timeseries data for this country</h3> : lineChart}
     </div>
   );
 }
